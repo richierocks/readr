bf8de9f169c44bd088db6c6baf68c565107272ea
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-06-16 11:02:21 +0300

    added na_strings utility fn

---
+++ R/na_strings.R
@@ -0,0 +1,20 @@
+#' Missing strings
+#'
+#' Missing string by data creation software (Excel, SAS, Stata, SQL currently
+#' supported).  Use this to set the \code{na.strings} argument in
+#' \code{\link{read_table}}.
+#' @param data_creator A string giving the data creation software
+#' @return A character vector.
+#' @examples
+#' na_strings("excel")
+#' @export
+na_strings <- function(data_creator = c("generic", "excel", "sas", "stata", "sql")) {
+  data_creator <- match.arg(data_creator)
+  switch(
+    data_creator,
+    unknown = "NA",
+    excel = c("#N/A", "#NULL!", "#NUM!", "#REF!", "#VALUE!", "#DIV/0!", "#NAME?"),
+    sas = ,
+    stata = ".",
+    sql = "NULL")
+}
